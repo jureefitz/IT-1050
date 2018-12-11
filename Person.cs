18af818c92932cb5aa9f7faf9927fe8433c7c218
jureefitz <42006462+jureefitz@users.noreply.github.com>
2018-12-10 22:07:57 -0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+
+        public string GetFullName()
+        {
+            return this.LastName + " " + this.FirstName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            Console.WriteLine("My name is " + this.GetFullName() + ".");
+            Console.WriteLine("My age is " + this.Age + ".");
+        }
+    }
+}
